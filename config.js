5bb9615f53dadc00a02c3b6f2d4bfb4e7f38490e
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-01-09 17:07:32 -0500

    Reorganized files behind lib and created index to expose the wrapper

--- config.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios').default;
-
-const httpService = async request => {
-    const response = await axios(request);
-    return response;
-};
-
-module.exports = httpService;
